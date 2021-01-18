9666199b8e198de9495523dbc627df27a535f62d
Pavel Sudomoin <sudomoin.pavel@yandex.ru>
2021-01-18 17:12:00 +0300

    add recipe models

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Unit, Recipe, Ingredient, IngredientAmount
+
+admin.site.register(Unit)
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(IngredientAmount)
--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Ingredient(models.Model):
+    ingredient = models.TextField()
+
+
+class Unit(models.Model):
+    unit = models.TextField()
+
+
+class Recipe(models.Model):
+    TAG_CHOICES = [
+        ('breakfast', 'завтрак'),
+        ('lunch', 'обед'),
+        ('dinner', 'ужин'),
+    ]
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes'
+    )
+    name = models.TextField()
+    image = models.ImageField(upload_to="recipes/", blank=True, null=True)
+    description = models.TextField()
+    ingredient = models.ManyToManyField(
+        Ingredient,
+        through='IngredientAmount',
+        related_name='recipes')
+    tag = models.CharField(choices=TAG_CHOICES, max_length=300)
+    time = models.TimeField(auto_now=False, auto_now_add=False)
+    slug = models.SlugField(unique=True)
+
+
+class IngredientAmount(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='ingredient_amounts'
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.PROTECT,
+        related_name='ingredient_amounts'
+    )
+    unit = models.ForeignKey(
+        Unit,
+        on_delete=models.PROTECT,
+        related_name='ingredient_amounts'
+    )
+    quantity = models.FloatField()
