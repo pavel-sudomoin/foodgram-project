575beec1c3fefd0ccead2aac2cdabfd8db939382
Pavel Sudomoin <sudomoin.pavel@yandex.ru>
2021-02-04 18:32:36 +0300

    add other templates for users

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -128,4 +128,5 @@ STATICFILES_DIRS = (
 
 # Login
 LOGIN_URL = "/auth/login/"
-LOGIN_REDIRECT_URL = "index"
+LOGIN_REDIRECT_URL = "main_page"
+LOGOUT_REDIRECT_URL = "main_page"
