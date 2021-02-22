78013a79e1b4dbf7c4881014f2d0989e4b98f898
Pavel Sudomoin <sudomoin.pavel@yandex.ru>
2021-02-22 23:25:45 +0300

    fix pep8

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,26 +1,10 @@
 from django import forms
-from django.utils.translation import gettext_lazy as _
 
-from .models import Recipe, Tag
+from .models import Recipe
 
 
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
-        fields = ["name", "image", 'description', 'tag', 'time']
+        fields = ['name', 'image', 'description', 'tag', 'time']
         exclude = ['ingredients']
-        #labels = {
-        #    "text": _("Текст поста"),
-        #    "group": _("Группа"),
-        #    "image": _("Изображение"),
-        #}
-        #help_texts = {
-        #    "text": _("Введите текст поста"),
-        #    "group": _("Выберите группу, к которой относится пост"),
-        #    "image": _("Выберите изображение"),
-        #}
-        #error_messages = {
-        #    "text": {
-        #        "required": _("Вы не добавили текст поста"),
-        #    },
-        #}
