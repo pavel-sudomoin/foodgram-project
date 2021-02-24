0d5e3b73ce41b3aec0655e97e840b326d1be8824
Pavel Sudomoin <sudomoin.pavel@yandex.ru>
2021-02-24 20:46:39 +0300

    fix quotes

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -5,15 +5,15 @@ from django.conf.urls import handler404, handler500
 from django.conf import settings
 from django.conf.urls.static import static
 
-handler404 = "recipes.views.page_not_found"
-handler500 = "recipes.views.server_error"
+handler404 = 'recipes.views.page_not_found'
+handler500 = 'recipes.views.server_error'
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/v1/", include("api.urls")),
-    path("", include("recipes.urls")),
-    path("auth/", include("users.urls")),
-    path("auth/", include("django.contrib.auth.urls")),
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('api.urls')),
+    path('', include('recipes.urls')),
+    path('auth/', include('users.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
     path('about/', include('about.urls', namespace='about')),
 ]
 
