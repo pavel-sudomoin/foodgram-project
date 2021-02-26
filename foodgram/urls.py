2033f7049768feab132f9e7dfad7228ef22a68bc
Pavel Sudomoin <sudomoin.pavel@yandex.ru>
2021-02-26 14:43:20 +0300

    reformatted by black

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -5,20 +5,18 @@ from django.conf.urls import handler404, handler500
 from django.conf import settings
 from django.conf.urls.static import static
 
-handler404 = 'recipes.views.page_not_found'
-handler500 = 'recipes.views.server_error'
+handler404 = "recipes.views.page_not_found"
+handler500 = "recipes.views.server_error"
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls')),
-    path('', include('recipes.urls')),
-    path('auth/', include('users.urls')),
-    path('auth/', include('django.contrib.auth.urls')),
-    path('about/', include('about.urls', namespace='about')),
+    path("admin/", admin.site.urls),
+    path("api/v1/", include("api.urls")),
+    path("", include("recipes.urls")),
+    path("auth/", include("users.urls")),
+    path("auth/", include("django.contrib.auth.urls")),
+    path("about/", include("about.urls", namespace="about")),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
