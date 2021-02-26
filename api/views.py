e0bdf859c63c1ed1b44765471476b92b2eb8ae08
Pavel Sudomoin <sudomoin.pavel@yandex.ru>
2021-02-26 14:24:25 +0300

    reformatted by black

--- api/views.py
+++ api/views.py
@@ -10,67 +10,69 @@ from recipes.models import Ingredient, Recipe
 User = get_user_model()
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def get_ingredients(request):
-    query = request.GET.get('query', None)
-    ingredients = [{'title': ing.name, 'dimension': ing.unit}
-                   for ing in Ingredient.objects.filter(name__icontains=query)]
+    query = request.GET.get("query", None)
+    ingredients = [
+        {"title": ing.name, "dimension": ing.unit}
+        for ing in Ingredient.objects.filter(name__icontains=query)
+    ]
     return Response(ingredients)
 
 
 @login_required
-@api_view(['POST'])
+@api_view(["POST"])
 def add_favorites(request):
-    recipe = get_object_or_404(Recipe, id=request.data.get('id'))
+    recipe = get_object_or_404(Recipe, id=request.data.get("id"))
     user = request.user
     if recipe.author == user:
-        return Response({'success': False})
+        return Response({"success": False})
     user.profile.favorites.add(recipe)
-    return Response({'success': True})
+    return Response({"success": True})
 
 
 @login_required
-@api_view(['DELETE'])
+@api_view(["DELETE"])
 def remove_favorites(request, recipe_id):
     recipe = get_object_or_404(Recipe, id=recipe_id)
     user = request.user
     user.profile.favorites.remove(recipe)
-    return Response({'success': True})
+    return Response({"success": True})
 
 
 @login_required
-@api_view(['POST'])
+@api_view(["POST"])
 def add_subscriptions(request):
-    author = get_object_or_404(User, id=request.data.get('id'))
+    author = get_object_or_404(User, id=request.data.get("id"))
     user = request.user
     if author == user:
-        return Response({'success': False})
+        return Response({"success": False})
     user.profile.following.add(author)
-    return Response({'success': True})
+    return Response({"success": True})
 
 
 @login_required
-@api_view(['DELETE'])
+@api_view(["DELETE"])
 def remove_subscriptions(request, author_id):
     author = get_object_or_404(User, id=author_id)
     user = request.user
     user.profile.following.remove(author)
-    return Response({'success': True})
+    return Response({"success": True})
 
 
 @login_required
-@api_view(['GET', 'POST'])
+@api_view(["GET", "POST"])
 def add_or_get_purchases(request):
-    recipe = get_object_or_404(Recipe, id=request.data.get('id'))
+    recipe = get_object_or_404(Recipe, id=request.data.get("id"))
     user = request.user
     user.profile.shoplist.add(recipe)
-    return Response({'success': True})
+    return Response({"success": True})
 
 
 @login_required
-@api_view(['DELETE'])
+@api_view(["DELETE"])
 def remove_purchases(request, recipe_id):
     recipe = get_object_or_404(Recipe, id=recipe_id)
     user = request.user
     user.profile.shoplist.remove(recipe)
-    return Response({'success': True})
+    return Response({"success": True})
