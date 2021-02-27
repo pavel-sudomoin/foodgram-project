08bbb5c79c635848a6ea65364741ef8043adcbdc
Pavel Sudomoin <sudomoin.pavel@yandex.ru>
2021-02-27 15:20:47 +0300

    delete settings

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
-from django.conf import settings
 
 from .generatereport import PDFReport
 
