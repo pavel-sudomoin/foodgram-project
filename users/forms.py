148c29826f4b287f59b8849f43fda2ee223eba9f
Pavel Sudomoin <sudomoin.pavel@yandex.ru>
2021-02-28 13:13:19 +0300

    add password validation and exclude static folder from root

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,15 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth import password_validation
+from django import forms
 
 User = get_user_model()
 
 
 class CreationForm(UserCreationForm):
+    password2 = None
+
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ("first_name", "username", "email")
@@ -15,7 +19,10 @@ class CreationForm(UserCreationForm):
             "email": _("Адрес электронной почты"),
         }
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields["password1"].label = "Пароль"
-        del self.fields["password2"]
+    def clean_password1(self):
+        password1 = self.cleaned_data.get("password1")
+        try:
+            password_validation.validate_password(password1, self.instance)
+        except forms.ValidationError as error:
+            self.add_error("password1", error)
+        return password1
